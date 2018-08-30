7db7491bf8bdef9b15d6947eca346e19bd854659
DJLionheart <aundario@gmail.com>
2018-08-30 13:27:41 -0600

    starting work on bill details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from './components/Header/Header';
+import BillDetails from './components/BillDetails/BillDetails';
 
 import './assets/styles/App.css';
 
@@ -8,7 +9,8 @@ class App extends Component {
   render() {
     return (
       <main className="blytz_app">
-        <Header/>        
+        <Header/>
+        <BillDetails/>       
       </main>
     );
   }
---
+++ src/components/BillDetails/BillDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BillDetails() {
+    return(
+        <section className="bill_details">
+            <h1>Bill Details</h1>
+        </section>
+    )
+}
