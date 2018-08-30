f6193ac6e0969afb9057945d7763df161df08dd5
DJLionheart <aundario@gmail.com>
2018-08-30 15:57:33 -0600

    starting payment component

--- src/components/BillDetails/BillDetails.js
+++ src/components/BillDetails/BillDetails.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function BillDetails() {
-    return(
-        <section className="bill_details">
-            <h1>Bill Details</h1>
-        </section>
-    )
+import businessLogo from '../../assets/img/Square-Enix-logo.png';
+
+import '../../assets/styles/comp/BillDetails.css';
+
+class BillDetails extends Component {
+    constructor() {
+        super();
+        this.state = {
+            businessLogo: businessLogo,
+            billAmount: 250,
+            recurring: true
+        }
+
+        this.toggleCheckbox = this.toggleCheckbox.bind(this);
+    }
+
+    toggleCheckbox() {
+        this.setState({
+            recurring: !this.state.recurring
+        })
+    }
+
+    componentDidMount() {
+        /*
+        Before the page finishes loading, a call could be made here to load all the pertinent bill info: business logo, amount, etc.
+        */
+    }
+
+    render() {
+        const { businessLogo, billAmount, recurring } = this.state;
+        return(
+            <article className="bill_details">
+                <section className="business_logo">
+                    <img src={businessLogo} alt="company logo"/>
+                </section>
+                <section className="bill_amount">
+                    <section className="shaded_box">
+                        <h5 id="bill_heading">Amount Due</h5>
+                        <h1 id="amount">${billAmount}.00</h1>
+                    </section>
+                    <span id="recurring_info">
+                        <label className="checkbox_container">
+                            <input type="checkbox" checked={recurring} onChange={ this.toggleCheckbox }/>
+                            <span className="checkmark"></span> 
+                        </label>
+                        Make Recurring Payment
+                            
+                    </span>
+                </section>
+            </article>
+        )
+    }
 }
+
+export default BillDetails;
