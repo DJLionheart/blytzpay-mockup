52980d341b0016af7898465c3a51ec72514657e8
DJLionheart <aundario@gmail.com>
2018-08-30 23:01:50 -0600

    working on styled component checkbox

--- src/components/BillDetails/BillDetails.js
+++ src/components/BillDetails/BillDetails.js
@@ -1,8 +1,93 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import businessLogo from '../../assets/img/Square-Enix-logo.png';
 
 import '../../assets/styles/comp/BillDetails.css';
+
+const Details = styled.article`
+    padding: 40px;
+    height: 33vh;
+    /* background-color: magenta; */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const BusinessLogo = styled.section`
+    width: 80%;
+`;
+
+const BillAmountSection = styled.section`
+    text-align: center;
+`;
+
+const ShadedBox = styled.section`
+    padding: 15px;
+    margin-bottom: 5px;
+    color: rgb(18, 230, 18);
+    height: 60px;
+    width: 65vw;
+    background-color: rgba(0, 0, 0, 0.198);
+    border-radius: 10px;
+`;
+
+const AmountHeading = styled.h5`
+    margin: 0;
+    font-size: 19px;
+    font-weight: 300;
+    line-height: 19pt;
+`;
+
+const TotalDueHeading = styled.h1`
+    margin: 0;
+    font-size: 35px;
+    font-weight: 600;
+    letter-spacing: 2pt;
+    line-height: 23pt;
+`;
+
+const CheckboxContainer = styled.label`
+    padding-left: 12px;
+    float: left;
+    display: block;
+    position: relative;
+    height: 18px;
+    width: 18px;
+    /* background-color: rgb(224, 224, 224); */
+`;
+
+const Checkbox = styled.input`
+    position: absolute;
+    opacity: 0;  
+`;
+
+const Checkmark = styled.span`
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 20px;
+    width: 20px;
+    background-color:rgb(195, 195, 195);
+    border-radius: 3px;
+    &:checked {
+        background-color:  rgb(18, 230, 18);
+    }
+
+    &::after {
+        content: "";
+        position: absolute;
+        display: none; 
+    }
+`;
+
+const RecurringInfo = styled.span`
+    display: flex;
+    justify-content: center;
+    color: white;
+    font-weight: 300;
+`;
 
 class BillDetails extends Component {
     constructor() {
@@ -31,25 +116,25 @@ class BillDetails extends Component {
     render() {
         const { businessLogo, billAmount, recurring } = this.state;
         return(
-            <article className="bill_details">
-                <section className="business_logo">
+            <Details>
+                <BusinessLogo>
                     <img src={businessLogo} alt="company logo"/>
-                </section>
-                <section className="bill_amount">
-                    <section className="shaded_box">
-                        <h5 id="bill_heading">Amount Due</h5>
-                        <h1 id="amount">${billAmount}.00</h1>
-                    </section>
-                    <span id="recurring_info">
-                        <label className="checkbox_container">
+                </BusinessLogo>
+                <BillAmountSection>
+                    <ShadedBox>
+                        <AmountHeading>Amount Due</AmountHeading>
+                        <TotalDueHeading>${billAmount}.00</TotalDueHeading>
+                    </ShadedBox>
+                    <RecurringInfo>
+                        <CheckboxContainer>
                             <input type="checkbox" checked={recurring} onChange={ this.toggleCheckbox }/>
                             <span className="checkmark"></span> 
-                        </label>
+                        </CheckboxContainer>
                         Make Recurring Payment
                             
-                    </span>
-                </section>
-            </article>
+                    </RecurringInfo>
+                </BillAmountSection>
+            </Details>
         )
     }
 }
